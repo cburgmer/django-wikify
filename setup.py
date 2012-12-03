e9b1dd633d8aa8fb532d956ee2914b1b392408ab
Christoph Burgmer <cburgmer@ira.uka.de>
2012-12-03 15:06:17 +0100

    Move dependencies into requirements.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
+import re
 import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+
+    return requirements
 
 setup(name="django-wikify",
       #version="0.1",
@@ -15,7 +30,7 @@ setup(name="django-wikify",
       dependency_links = [
           "http://google-diff-match-patch.googlecode.com/svn/trunk/python2/diff_match_patch.py#egg=diff_match_patch-py2_svn"
       ],
-      install_requires = ['django_reversion>=1.6', 'Django', 'diff_match_patch'],
+      install_requires = parse_requirements('requirements.txt'),
       tests_require = ['lxml', 'cssselect', 'fudge'],
       test_suite = "example.runtests.runtests",
       classifiers=["Development Status :: 3 - Alpha",
