d0da96c8e4a5d106b50f04531d1bb7045167fee5
Christoph Burgmer <cburgmer@ira.uka.de>
2012-12-04 10:12:14 +0100

    Deployment to Heroku by Travis

---
+++ heroku_example/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+GIT_COMMIT_HASH=`git log | head -1 | sed "s/commit //"`
+
+# Set-up heroku stuff
+gem install heroku
+
+echo "Host heroku.com" >> ~/.ssh/config
+echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+echo "   CheckHostIP no" >> ~/.ssh/config
+echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+
+heroku keys:clear
+yes | heroku keys:add
+
+# Prepare requirements for Heroku app
+cat requirements.template | sed "s/\\\$GIT_COMMIT_HASH\\\$/$GIT_COMMIT_HASH/" > requirements.txt
+
+# Receipt from http://stackoverflow.com/questions/7539382/how-can-i-deploy-from-a-git-subdirectory
+git init
+git add .
+git commit -m "Git commit $GIT_COMMIT_HASH"
+git remote add heroku git@heroku.com:agile-eyrie-2365.git
+
+# Merge with heroku copy
+git pull heroku master
+git checkout --ours .
+git add -u
+git commit -m "merged"
+
+git push heroku master
+rm -rf .git
